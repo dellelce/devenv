3ffbe453ee258a23194a2fd030d82f1897bdb7e1
Antonio Dell'Elce <antouk@dellelce.com>
2020-05-04 14:22:56 +0100

    add "kill_generic": kill "generic" container

--- mk/generic.mk
+++ mk/generic.mk
@@ -11,8 +11,12 @@ exec_generic:
 clean_generic:
 	@cd generic; make clean
 
+kill_generic:
+	@cd generic; make kill
+
 # $HELP$
 # build_generic       build generic container
 # run_generic         run container (interactive mode)
 # exec_generic        attaching to running "generic container"
+# kill_generic        kill "generic" container
 # clean_generic       run target "clean"
